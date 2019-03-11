58fc1bfcb60e8795c7160cbf65f3c3173efa8087
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-03-10 21:01:42 -0700

    Updates

---
+++ precache-manifest.da4ec40a98e025c3a61d992030ee8b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/[VelazquezLuis].github.io/[Portfolio]/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "deab9030e5ccf54565bb",
+    "url": "/[VelazquezLuis].github.io/[Portfolio]/static/js/runtime~main.c8398837.js"
+  },
+  {
+    "revision": "76dbdcb018aa46b22bb8",
+    "url": "/[VelazquezLuis].github.io/[Portfolio]/static/js/main.a1777560.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/[VelazquezLuis].github.io/[Portfolio]/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "76dbdcb018aa46b22bb8",
+    "url": "/[VelazquezLuis].github.io/[Portfolio]/static/css/main.08692d9b.chunk.css"
+  },
+  {
+    "revision": "bf483f65d5380b8224dcf773ff2d1cb6",
+    "url": "/[VelazquezLuis].github.io/[Portfolio]/index.html"
+  }
+];
