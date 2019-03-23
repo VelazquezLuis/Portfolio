3acbcf09d6fcac049b5ea65abacf0bb2b0292bec
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-03-22 18:34:37 -0700

    added fiexed navbar

--- src/components/header/nav/Nav.js
+++ src/components/header/nav/Nav.js
@@ -2,39 +2,34 @@ import React, {Component} from 'react';
 import './Nav.css';
 
 class Nav extends Component {
-  
+
+
   render() {
     return(
 
-        <div class="pos-f-t">          
-          <nav class="navbar navbar-dark bg-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <p className="white">Luis Javier velazquez</p>
-          </nav>
-          
-          <div class="collapse" id="navbarToggleExternalContent">
-            <div class="bg-dark p-4">
-              <ul class=" nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link active" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-              </ul>
-            </div>
-          </div>            
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Fixed navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
         </div>
-          
-                
+      </nav>
      
     );
   }
@@ -42,28 +37,33 @@ class Nav extends Component {
 
 export default Nav;
 
-
-// <a  data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-// <img class="img-thumbnail"  src={mo} alt="menuIcon"></img>
-// </a>
-
-// <div class="collapse" id="collapseExample">
-// <div class="card card-body">
-
-// <ul class="nav justify-content-end">
-// <li class="nav-item">
-// <a class="nav-link active" href="#">Active</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="#">Link</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="#">Link</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link disabled" href="#">Disabled</a>
-// </li>
-// </ul>
-
-// </div>
-// </div>
+// <div class="pos-f-t">          
+//           <nav class="navbar navbar-dark bg-dark">
+//             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+//               <span class="navbar-toggler-icon"></span>
+//             </button>
+//             <p className="white">Luis Javier velazquez</p>
+//             <div class="collapse" id="navbarToggleExternalContent">
+//             <div class="bg-dark p-4">
+//               <ul class=" nav flex-column flex-lg-row">
+//                 <li class="nav-item">
+//                   <a class="nav-link active" href="#">Active</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">Link</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">Link</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link disabled" href="#">Disabled</a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>   
+//           </nav>
+          
+                   
+//         </div>
+          
+                
