a518c528a3a25e1ee329ba447815b9bfcc838ec0
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-03-24 10:22:38 -0700

    added raw footer section

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+
+
+  render() {
+    return(
+
+      <footer class="footer mt-auto py-3">
+        <div class="container">
+          <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+      </footer>
+      
+      );
+    }
+  }
+  
+  export default Footer;
