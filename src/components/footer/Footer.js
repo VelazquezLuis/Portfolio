3f1e55fdf57f91164e3082cbe4da0a5c720e8c80
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-03-26 22:28:54 -0700

    played with boostrap footer.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,12 +6,12 @@ class Footer extends Component {
 
   render() {
     return(
-
+      
         <footer class="footer mt-auto py-3">
           <div class="container">
             <span class="text-muted">Place sticky footer content here.</span>
           </div>
-        </footer>      
+        </footer>
       );
     }
   }
