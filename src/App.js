4310b96106fc965ccf740cf8b1b4b246b1957f0f
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-03-28 15:22:34 -0700

    deleted repeated css

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="container">
+        <header className="container ">
         <Nav/>
         </header>
         <Head/>
